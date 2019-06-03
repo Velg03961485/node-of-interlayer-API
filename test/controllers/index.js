c3cfef440000ebd39e2b9bbc54590a2b6d42ba5a
velg <velg@velg.local>
2019-06-03 23:11:35 +0800

    velg commit

---
+++ test/controllers/index.js
@@ -0,0 +1,11 @@
+var request = require('request');
+
+request({
+    url: 'http://crm.ibetwo.com:8030/common/getStrore',
+    method: 'POST',
+}, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        //输出返回的内容
+        console.log(body);
+    }
+});
