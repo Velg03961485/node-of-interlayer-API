626c05cfca9fbf5820eb9caaaba468432acd98c2
e <liuxu@yunfucloud.com>
2019-06-05 18:04:20 +0800

    velg commit post

--- test/controllers/post.js
+++ test/controllers/post.js
@@ -1,5 +1,12 @@
 module.exports = function(app){
     console.log('hello word');
     var async = require('async');
-
+    // app.get('/post/getList',function(req, res){
+        
+    //     try{
+    //         console.log(req.body);
+    //     }catch(e){
+    //         console.log(e);
+    //     }
+    // })
 }
